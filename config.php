0426ddc4b418870307b20cdb93005f45cf5149cc
unknown <purvi.harniya@gmail.com>
2021-04-06 23:08:22 +0530

    initial setup

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+ob_start();
+session_start();
+$timezone = date_default_timezone_set("Asia/Kolkata");
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "cms";
+$con = mysqli_connect($servername, $server_user, $server_pass, $dbname);
+
+if (mysqli_connect_errno()) {
+    echo "Could not connect to the database:" . mysqli_connect_errno();
+}
