e614b20b1b67b78aac58474d7384f0876a3e59cd
unknown <purvi.harniya@gmail.com>
2021-04-17 15:34:12 +0530

    static pages

---
+++ menu.php
@@ -0,0 +1,204 @@
+<?php
+
+include "shared/header.php";
+
+?>
+
+<!--Page header & Title-->
+<section id="page_header">
+    <div class="page_title">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="title">Menu</h2>
+                    <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!--Pricings-->
+<section id="pricing" class="padding-top">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2 class="heading">Hot beverages</h2>
+                <hr class="heading_space">
+            </div>
+            <div class="col-sm-6">
+                <div class="price padding-bottom">
+                    <div class="price_body">
+                        <ul class="pricing_feature">
+                            <li>Chocolate mint cappuccino<strong>&#8377;240</strong> </li>
+                            <li>Panna cotta cappuccino <strong>&#8377;220</strong></li>
+                            <li>Creamy cashew gingersnap latte<strong>&#8377;250</strong> </li>
+                            <li>Cinnamon hot chocolate <strong>&#8377;270</strong></li>
+                            <li>Classic hot chocolate <strong>&#8377;320</strong></li>
+                            <li>Authentic mexican hot chocolate <strong>&#8377;290</strong></li>
+                            <li>Green tea latte <strong>&#8377;210</strong></li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="price padding-bottom">
+                    <div class="price_body">
+                        <ul class="pricing_feature">
+                            <li>Chamomile tea <strong>&#8377;140</strong></li>
+                            <li>Indian masala chai <strong>&#8377;150</strong></li>
+                            <li>Vanilla custard strawberry crumbles<strong>&#8377;240</strong></li>
+                            <li>Kinto unitea<strong>&#8377;140</strong> </li>
+                            <li>Smokey harvest apple cider margrita<strong>&#8377;250</strong></li>
+                            <li>Smokey watermelon fizz<strong>&#8377;250</strong></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <h2 class="heading">Cold beverages</h2>
+                <hr class="heading_space">
+            </div>
+            <div class="col-sm-6">
+                <div class="price padding-bottom">
+                    <div class="price_body">
+                        <ul class="pricing_feature">
+                            <li>Rose and ginger paloma <strong>&#8377;160</strong></li>
+                            <li>Watermelon Rose margrita <strong>&#8377;240</strong></li>
+                            <li>French blackberry <strong>&#8377;250</strong></li>
+                            <li>Paloma grapefruit cocktail<strong>&#8377;250</strong> </li>
+                            <li>Cherry vodka soda<strong>&#8377;250</strong> </li>
+                            <li>Sparkling Strawberry Cucumber Mocktail <strong>&#8377;250</strong></li>
+                            <li>Berry berry paloma <strong>&#8377;250</strong></li>
+                            <li>Mint Lychee Collins <strong>&#8377;250</strong></li>
+                            <li>Rose Lemonade <strong>&#8377;250</strong></li>
+                            <li>Andremedaa<strong>&#8377;250</strong></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="price padding-bottom">
+                    <div class="price_body">
+                        <ul class="pricing_feature">
+                            <li>Strawberry hibiscus and mint iced tea<strong>&#8377;170</strong></li>
+                            <li>Triple citrus matcha lemonade<strong>&#8377;160</strong></li>
+                            <li>Cold brew coffee<strong>&#8377;140</strong></li>
+                            <li>Cold cocca coffee<strong>&#8377;180</strong></li>
+                            <li>Summer cocktail<strong>&#8377;199</strong></li>
+                            <li>Frozen Coconut caramel frappe<strong>&#8377;230</strong></li>
+                            <li>Honey lavendar cold brew<strong>&#8377;210</strong></li>
+                            <li>Pear Lemon Fizz<strong>&#8377;280</strong></li>
+                            <li>Cranberry blood orange<strong>&#8377;199</strong> </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <h2 class="heading">Mini Bites</h2>
+                <hr class="heading_space">
+            </div>
+            <div class="col-sm-6">
+                <div class="price padding-bottom">
+                    <div class="price_body">
+                        <ul class="pricing_feature">
+                            <li>Mushroom cheese pots<strong>&#8377;300</strong> </li>
+                            <li>Veggie rolls<strong>&#8377;240</strong> </li>
+                            <li>Steamed dumplings <strong>&#8377;240</strong> </li>
+                            <li>Olive crostini<strong>&#8377;340</strong> </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="price padding-bottom">
+                    <div class="price_body">
+                        <ul class="pricing_feature">
+                            <li>Sweet potato crostini <strong>&#8377;320</strong></li>
+                            <li>Parsley cheese nuggets <strong>&#8377;270</strong></li>
+                            <li>Ceasar salad wonton<strong>&#8377;240</strong> </li>
+                            <li>Za'tar Cheese galettes<strong>&#8377;240</strong> </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <h2 class="heading">Desserts</h2>
+                <hr class="heading_space">
+            </div>
+            <div class="col-sm-6">
+                <div class="price padding-bottom">
+                    <div class="price_body">
+                        <ul class="pricing_feature">
+                            <li>Blueberry pancakes<strong>&#8377;140</strong></li>
+                            <li>Chocolate hazelnut tart <strong>&#8377;140</strong></li>
+                            <li>Black bean chocolate mousse <strong>&#8377;140</strong></li>
+                            <li>Coconut yogurt chia parfait<strong>&#8377;140</strong> </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="price padding-bottom">
+                    <div class="price_body">
+                        <ul class="pricing_feature">
+                            <li>Nutella cookie cheesecake<strong>&#8377;190</strong> </li>
+                            <li>Salted Brownies<strong>&#8377;200</strong> </li>
+                            <li>Blackberry cheesecake jars<strong>&#8377;250</strong></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <h2 class="heading">Appetizers</h2>
+                <hr class="heading_space">
+            </div>
+            <div class="col-sm-6">
+                <div class="price padding-bottom">
+                    <div class="price_body">
+                        <ul class="pricing_feature">
+                            <li>Cheese baked pasta <strong>&#8377;250</strong></li>
+                            <li>Marghetira pizza<strong>&#8377;300</strong> </li>
+                            <li>Sun-dried tomato pesto pasta <strong>&#8377;340</strong></li>
+                            <li>Exotic burger<strong>&#8377;270</strong> </li>
+                            <li>Layered Club sandwich <strong>&#8377;199</strong></li>
+                            <li>Bulgogi sandwich with kimchi mayo <strong>&#8377;240</strong></li>
+                            <li>Cream cheese sandwich <strong>&#8377;180</strong></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="price padding-bottom">
+                    <div class="price_body">
+                        <ul class="pricing_feature">
+                            <li>Focaccia <strong>&#8377;310</strong> </li>
+                            <li>French toast <strong>&#8377;260</strong> </li>
+                            <li>Cheesy korean garlic bread <strong>&#8377;240</strong></li>
+                            <li>Berry croissants <strong>&#8377;190</strong> </li>
+                            <li>Coleslaw sandwich <strong>&#8377;210</strong></li>
+                            <li>Fusilli pasta <strong>&#8377;230</strong> </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!--Footer-->
+<?php
+
+include "shared/footer.php";
+
+?>
