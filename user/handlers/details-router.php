c7d32ffaf5890f748966faaf23384a8f42a75386
unknown <purvi.harniya@gmail.com>
2021-04-20 11:45:44 +0530

    minor backend changes

--- user/handlers/details-router.php
+++ user/handlers/details-router.php
@@ -4,11 +4,17 @@ $user_id = $_SESSION['user_id'];
 
 $name = htmlspecialchars($_POST['name']);
 $username = htmlspecialchars($_POST['username']);
-$password = htmlspecialchars($_POST['password']);
 $phone = $_POST['phone'];
 $email = htmlspecialchars($_POST['email']);
 $address = htmlspecialchars($_POST['address']);
-$sql = "UPDATE users SET name = '$name', username = '$username', password='$password', contact=$phone, email='$email', address='$address' WHERE id = $user_id;";
+
+if ($_POST['password'] != '') {
+    $password = htmlspecialchars($_POST['password']);
+    $sql = "UPDATE users SET name = '$name', username = '$username', password='$password', contact=$phone, email='$email', address='$address' WHERE id = $user_id;";
+} else {
+    $sql = "UPDATE users SET name = '$name', username = '$username', contact=$phone, email='$email', address='$address' WHERE id = $user_id;";
+}
+
 if ($con->query($sql) == true) {
     $_SESSION['name'] = $name;
 }
